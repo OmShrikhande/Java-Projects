d440f26b7269ad77262fbf2126846100535e499a
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-03 16:01:39 +0530

    Updateion method added

--- First.java
+++ First.java
@@ -72,7 +72,7 @@ public class First {
 
 class CRUDproj{
 	//declaration
-	String pname,pdes,date, deadline, refs, choice="";
+	String pname,pdes,date, deadline, refs, choice="",choices;
 	
 	//imports
 	Scanner sc = new Scanner(System.in);
@@ -80,9 +80,6 @@ class CRUDproj{
 	SimpleDateFormat dateformat= new SimpleDateFormat("dd/MM/YY"); 
 	Concolrs col =new Concolrs();
 	
-	public void IntroOfProj() {
-		
-	}
 	 public void Createproj() {
 		
 	System.out.print(col.ANSI_PURPLE+"Enter name of the project: "+col.ANSI_RESET);
@@ -102,15 +99,62 @@ class CRUDproj{
 	 //to update any project information
 	 public void Updateproj() {
 		 
+		 
+		 	
+		 
+			//Giving choices for updation
+			System.out.println(col.ANSI_YELLOW+"What do you want to upate?? "+col.ANSI_RESET);
+			System.out.println(col.ANSI_PURPLE+"A. Project name ");
+			System.out.println("B. Project Description ");
+			System.out.println("C. Deadline ");
+			System.out.println("D. Project Refrences");
+			System.out.println("E. Project List"+col.ANSI_RESET);
+			
+			
+			
+			System.out.print(col.ANSI_GREEN+"\n\nEnter any one of the choice: "+col.ANSI_RESET);
+			choices = sc.nextLine();
+			
+			switch(choices.toUpperCase()) {
+			case "A":
+				System.out.println("Enter new Project name: ");
+				pname = sc.nextLine();
+				break;
+			case "B":
+				System.out.println("Enter new Description: ");
+				pdes = sc.nextLine();
+				break;
+			case "C":
+				System.out.println("Enter new Deadline: ");
+				deadline = sc.nextLine();
+				break;
+			case "D":
+				System.out.println("Enter new Refrences: ");
+				refs = sc.nextLine();
+				break;
+			case "E":
+				Lookproj();
+				break;
+				
+			default:
+				System.out.println("Enter proper choice");
+				break;
+			}
+			
+		
+			
+			System.out.println("\n--------------------------------------------------------------------------");
+			 
 	 }
 	 
 	 //to see the projects that i have 
 	 public void Lookproj() {
-		 
+		 System.out.println("Under Devlopment");
 	 }
 	 
 	 //to delete any project
 	 public void Delproj() {
+		 System.out.println("Under Devlopment");
 		 
 	 }
 }
