3e9aea54ca131982076cec7570ec9dc89dd457f0
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-10 23:43:17 +0530

    desc added

--- korona/projkorona.java
+++ korona/projkorona.java
@@ -4,6 +4,7 @@ public class projkorona {
 
 	public static void main(String[] args) {
 		System.out.println("this is first running stage");
+		System.out.println("simply this project is the extended version of the project management system.");
 
 	}
 
