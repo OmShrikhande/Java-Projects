ce392a48d95e9cb31d6e08078894dd819c500b9b
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-13 23:53:26 +0530

    taking input

--- korona/Index.java
+++ korona/Index.java
@@ -1,10 +1,13 @@
 package jproj.korona;
 
+import java.util.Scanner;
 public class Index {
 
 	public static void main(String[] args) {
 		System.out.println("enter the name of the patient: ");
-
+		String pname;
+		Scanner sc = new Scanner(System.in);
+		pname = sc.next();
 	}
 
 }
